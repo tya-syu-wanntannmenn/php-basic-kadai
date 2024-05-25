8568da96eb025effa047454f4dcf687680ba3dfd
tya-syu-wanntannmenn <157363231+tya-syu-wanntannmenn@users.noreply.github.com>
2024-05-25 13:30:05 +0900

    フォルダをアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_sort</title>
+</head>
+
+<body>
+    <p>
+        <?php
+          $nums = [15, 4, 18, 23, 10];
+          
+          function sort_2way($array, $order) {
+            if($order) {
+              echo '昇順にソートします。<br>';
+              sort($array);
+              foreach($array as $num) { 
+                echo "{$num}<br>";
+              }
+            } else {
+              echo '降順にソートします。<br>';
+              rsort($array);
+              foreach($array as $num) { 
+                echo "{$num}<br>";
+              }
+            }
+          }
+
+          sort_2way($nums, true); 
+          sort_2way($nums, false);
+        ?>
+    </p>
+</body>
+
+</html>
